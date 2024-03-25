f3dd76a94ae441ab0f7c4271a5706f59152fdadb
ChaesongYun <139418987+ChaesongYun@users.noreply.github.com>
2024-03-25 21:51:23 +0900

    p영어끝말잇기

---
+++ 해시/p영어끝말잇기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0 ,0};
+        HashMap<String, Integer> sMap = new HashMap<String, Integer>();
+        sMap.put(words[0], 0);
+
+        for(int i = 1; i < words.length; i++) {
+            sMap.put(words[i], 0);
+            System.out.println(sMap.size());
+            if (words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)
+                || sMap.size() < i + 1) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
